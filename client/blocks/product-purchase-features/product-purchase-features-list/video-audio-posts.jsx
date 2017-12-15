1f268093e7c63e3e4abca77bfe0e330fc8c495ad
Jeremey <jeremey.duvall@automattic.com>
2017-12-15 13:58:07 -0700

    Plans: Update storage limit for Business plan
    The storage plan is actually unlimited (not 10GB) for the Business plan.
    This updates the plan features page to reflect the unlimited storage.
    
    Fixes: #20759

--- client/blocks/product-purchase-features/product-purchase-features-list/video-audio-posts.jsx
+++ client/blocks/product-purchase-features/product-purchase-features-list/video-audio-posts.jsx
@@ -10,17 +10,35 @@ import { localize } from 'i18n-calypso';
  */
 import PurchaseDetail from 'components/purchase-detail';
 import { newPost } from 'lib/paths';
+import { PLAN_BUSINESS, PLAN_PREMIUM } from 'lib/plans/constants';
 
-export default localize( ( { selectedSite, translate } ) => {
+export default localize( ( { selectedSite, plan, translate } ) => {
+	let featureDescription;
+
+	switch ( plan ) {
+		case PLAN_BUSINESS:
+			featureDescription = translate(
+				'Enrich your posts with video and audio, uploaded directly on your site. ' +
+					'The Business plan also adds unlimited file storage.'
+			);
+			break;
+		case PLAN_PREMIUM:
+			featureDescription = translate(
+				'Enrich your posts with video and audio, uploaded directly on your site. ' +
+					'The Premium plan also adds 10GB of file storage.'
+			);
+			break;
+		default:
+			featureDescription = '';
+			break;
+	}
+
 	return (
 		<div className="product-purchase-features-list__item">
 			<PurchaseDetail
 				icon={ <img src="/calypso/images/upgrades/media-post.svg" /> }
 				title={ translate( 'Video and audio posts' ) }
-				description={ translate(
-					'Enrich your posts with video and audio, uploaded directly on your site. ' +
-						'No ads or limits. The Premium plan also adds 10GB of file storage.'
-				) }
+				description={ featureDescription }
 				buttonText={ translate( 'Start a new post' ) }
 				href={ newPost( selectedSite ) }
 			/>
